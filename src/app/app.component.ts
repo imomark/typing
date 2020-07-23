eb09aa9abc5ab916bf964cdff445b8deeb314cd0
imomark <khanomarkhalid@gmail.com>
2020-07-23 20:01:11 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {lorem} from 'faker';
+import { getCurrencySymbol } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+
+  randomText = lorem.sentence();
+  inputText = '';
+  isVisible = false;
+
+  getInputText(value){
+    this.inputText = value;
+    console.log(this.inputText);
+
+    if(this.inputText === this.randomText){
+      this.isVisible = true;
+    }
+    else
+    this.isVisible =false;
+
+  }
+  compare(randomLetter: string, generatedLetter: string){
+
+    if(!generatedLetter)
+      return "pending";
+    if(randomLetter === generatedLetter)
+      return "correct";
+    else  return "incorrect";
+
+  }
 }
